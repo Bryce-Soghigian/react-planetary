1665ba5e72a8e2816129b1f058d97fe48b073870
Bryce-Soghigian <brybrycodingguy@gmail.com>
2020-03-18 13:57:24 -0500

    added saturn A

--- react-planetary-client/src/App.js
+++ react-planetary-client/src/App.js
@@ -1,23 +1,12 @@
 import React from 'react';
-import Moon from './components/Moon'
-import Earth from './components/Earth';
-import Venus from './components/Venus';
-import Mercury from './components/Mercury';
 import Mars from './components/Mars';
-import Jupiter from './components/Jupiter';
-import Neptune from './components/Neptune';
+import Saturn from './components/Saturn';
 
 function App() {
   return (
     <div >
-      <Mercury/>
-      <Venus/>
-      <Earth/>
-      <Moon/>
       <Mars/>
-      <Jupiter/>
-      <Neptune/>
-      
+      <Saturn/>
       
 
     </div>
